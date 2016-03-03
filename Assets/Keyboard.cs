2d2c3da0136619d3d6e3215b1e966c38e4154266
Aaron Hong <hong.aaron.c@gmail.com>
2016-03-03 09:47:47 -0800

    First commit

---
+++ Assets/Keyboard.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.ComponentModel;
+
+public class Keyboard : MonoBehaviour {
+
+    public string data;
+    public GameObject keycapPrefab;
+
+    private Keycap lastKeycap;
+
+	void Start () {
+        string[] rows = data.Split("[".ToCharArray(), System.StringSplitOptions.RemoveEmptyEntries);
+        string[] newRows = new string[rows.Length];
+        for (int i=0; i<rows.Length; i++)
+        {
+            newRows[i] = rows[i].Replace("], ", "").Replace("]", "");
+        }
+        rows = newRows;
+        for (int i=0; i<rows.Length; i++) {
+            GameObject newRowGO = new GameObject("Row" + i);
+            newRowGO.transform.parent = transform;
+            string[] keys = rows[i].Split(",".ToCharArray(), System.StringSplitOptions.RemoveEmptyEntries);
+
+            //Reformat keys
+            ArrayList newKeys = new ArrayList();
+            for (int j = 0; j < keys.Length; j++)
+            {
+                string newKey = keys[j];
+
+                //Check for multiple properties
+                if (keys[j].StartsWith("{") && !keys[j].EndsWith("}"))
+                {
+                    j++;
+                    while (!keys[j].EndsWith("}"))
+                    {
+                        newKey += "," + keys[j];
+                        j++;
+                    }
+                    newKey += "," + keys[j];
+                }
+                newKeys.Add(newKey);
+            }
+            keys = new string[newKeys.Count];
+            newKeys.CopyTo(keys);
+            for (int j = 0; j < keys.Length; j++)
+            {
+                if (keys[j].StartsWith("{") && keys[j].EndsWith("}"))
+                {
+                    //Create new keycap
+                    GameObject newKeycap = (GameObject)Instantiate(keycapPrefab, Vector3.zero, Quaternion.identity);
+                    newKeycap.transform.parent = newRowGO.transform;
+                    Keycap newKeycapProperties = newKeycap.GetComponent<Keycap>();
+                    newKeycapProperties.contents = keys[j+1].Replace("\"", "");
+                    //Set default properties
+                    SetDefaultProperties(newKeycapProperties, j == 0);
+
+                    //Read in new properties
+                    string newKeyString = keys[j].Replace("{", "").Replace("}", "");
+                    string[] properties = newKeyString.Split(",".ToCharArray());
+                    newKeycapProperties.rawData = properties;
+                    
+                    //Set new properties
+                    foreach (string property in properties)
+                    {
+                        System.Reflection.FieldInfo member = newKeycapProperties.GetType().GetField(property.Split(":".ToCharArray())[0]);
+                        if (member != null)
+                        {
+                            System.Type valueType = member.FieldType;
+                            string valueString = property.Split(":".ToCharArray())[1];
+                            TypeConverter tc = TypeDescriptor.GetConverter(valueType);
+                            object valueObj = tc.ConvertFromString(valueString);
+                            member.SetValue(newKeycapProperties, valueObj);
+                        }
+                        else
+                        {
+                            print("Could not find property: " + property);
+                        }
+                    }
+                    lastKeycap = newKeycapProperties;
+                    j++;
+                }
+                else if (keys[j].StartsWith("\"") && keys[j].EndsWith("\""))
+                {
+                    //Create new keycap
+                    GameObject newKeycap = (GameObject)Instantiate(keycapPrefab, Vector3.zero, Quaternion.identity);
+                    newKeycap.transform.parent = newRowGO.transform;
+                    Keycap newKeycapProperties = newKeycap.GetComponent<Keycap>();
+                    newKeycapProperties.contents = keys[j].Replace("\"", "");
+                    //Set default properties
+                    SetDefaultProperties(newKeycapProperties, j == 0);
+
+                    lastKeycap = newKeycapProperties;
+                }
+            }
+        }
+	}
+
+    void SetDefaultProperties(Keycap keycapProperties, bool newLine)
+    {
+        if (lastKeycap == null)
+            return;
+
+        lastKeycap.Calculate();
+
+        if (newLine)
+        {
+            keycapProperties.defaultX = 0.0f;
+            keycapProperties.defaultY = lastKeycap.y + 1.0f;
+        }
+        else
+        {
+            keycapProperties.defaultX = lastKeycap.x + lastKeycap.w;
+            keycapProperties.defaultY = lastKeycap.y;
+        }
+        keycapProperties.c = lastKeycap.c;
+    }
+}
---
+++ Assets/Keycap.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keycap : MonoBehaviour {
+    public string[] rawData;
+
+    public string contents;
+    public float defaultX = 0;
+    public float defaultY = 0;
+    public float x = 0;
+    public float y = 0;
+    public float w = 1;
+    public float h = 1;
+    public string c = "#FFFFFF";
+
+	void Start () {
+        //Flip horizontally
+        x *= -1;
+        w *= -1;
+
+        //Position
+        transform.position = new Vector3(0.5f * (w - 1) + x, 0, 0.5f * (h - 1) + y);
+
+        //Scale
+        transform.localScale = new Vector3(w, 1, h);
+
+        //Color
+        c = c.TrimStart("#\" ".ToCharArray());
+        byte r = byte.Parse(c.Substring(0,2), System.Globalization.NumberStyles.HexNumber);
+	    byte g = byte.Parse(c.Substring(2,2), System.Globalization.NumberStyles.HexNumber);
+	    byte b = byte.Parse(c.Substring(4,2), System.Globalization.NumberStyles.HexNumber);
+        Color newColor = new Color32(r,g,b,255);
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material.color = newColor;
+        }
+	}
+
+    public void Calculate()
+    {
+        x += defaultX;
+        y += defaultY;
+    }
+}
